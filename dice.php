8ea28ee451243c5418b84a16af6c9687c7588f2f
Jocelyn GEZ <jocelyn.gez@gmail.com>
2022-01-14 10:41:26 +0100

    Ajout de boucle
    - Avec une boucle on lance le dé 3 fois

--- dice.php
+++ dice.php
@@ -1,5 +1,18 @@
 <?php
 
-$n = random_int(1, 6);
+$numbers = [];
+$itération = 3;
 
-echo "Vous avez tiré un $n<br>\n";
+for ($i = 0 ; $i < $itération ; $i++){
+
+    $numbers[] = random_int(1, 6);
+
+}
+
+echo "Vous avez tiré :<br>\n";
+
+foreach ($numbers as $number){
+
+    echo "- un $number<br>\n";
+
+}
